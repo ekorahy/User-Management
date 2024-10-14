e4314726da7ebdaa8c8a500cf83f0299f3dc1529
ekorahy <ekorahy@gmail.com>
2024-10-14 16:36:53 +0700

    feat: add protection to account page

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -1,11 +1,18 @@
 import { createClient } from '@/utils/supabase/server';
 import AccountForm from './account-form';
+import { redirect } from 'next/navigation';
 
 export default async function Account() {
   const supabase = createClient();
 
   const {
     data: { user },
+    error,
   } = await supabase.auth.getUser();
+
+  if (error || !user) {
+    redirect('/login');
+  }
+
   return <AccountForm user={user} />;
 }
